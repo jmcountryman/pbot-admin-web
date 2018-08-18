e6e816131ddb748ec422f160175d09fe7baf49de
Jamie Countryman <Jamie.Countryman@gmail.com>
2018-08-18 18:19:08 -0500

    Add Bulma

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
 
 const App = function App()
 {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit
-        {' '}
-        <code>src/App.js</code>
-        {' '}
-        and save to reload.
-      </p>
-    </div>
+    <section className="section">
+      <div className="container">
+        <h1 className="title">
+          Hello World
+        </h1>
+        <p className="subtitle">
+          My first website with
+          {' '}
+          <strong>Bulma</strong>
+          !
+        </p>
+      </div>
+    </section>
   );
 };
 
