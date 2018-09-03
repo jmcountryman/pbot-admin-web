1ae6cf8720e8bf8dadd7d79fe82a57b9dfd58901
Jamie Countryman <Jamie.Countryman@gmail.com>
2018-09-02 20:34:22 -0500

    Move oauth step 2 to Rails

--- src/auth/auth-return.js
+++ src/auth/auth-return.js
@@ -19,17 +19,9 @@ class AuthReturn extends Component
 
     if (query.code)
     {
-      const data = queryString.stringify({
-        grant_type: 'authorization_code',
-        code: query.code,
-        redirect_uri: `${window.location.origin}${window.location.pathname}`,
-        scope: 'identify guilds',
-        client_id: '431216339894534145',
-        client_secret: 'LaJLtKLXpxGrP2h8-w2EOJoK27TIhnqh',
-      });
-
-      axios.post('https://discordapp.com/api/oauth2/token', data).then(() => (
-        this.setState({ message: 'Signed in with Discord!' })
+      // TODO: this should be a redux action/saga
+      axios.post('/api/auth', { code: query.code }).then(response => (
+        this.setState({ message: JSON.stringify(response.data) })
       ));
     }
   }
