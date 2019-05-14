3d1ae42f9570be72ce2aeb4cb49fa7b0508a3a38
Jamie Countryman <Jamie.Countryman@gmail.com>
2019-05-13 19:47:08 -0500

    Prod oauth URL

--- src/components/auth/sign-in-button.js
+++ src/components/auth/sign-in-button.js
@@ -9,11 +9,11 @@ const SignInButton = function SignInButton()
     <a
       className="button is-large has-text-white has-background-blurple"
       // local dev:
-      href="https://discordapp.com/api/oauth2/authorize?client_id=431216339894534145&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fauth%2Freturn&response_type=code&scope=identify%20guilds"
+      // href="https://discordapp.com/api/oauth2/authorize?client_id=431216339894534145&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fauth%2Freturn&response_type=code&scope=identify%20guilds"
       // local prod:
       // href="https://discordapp.com/api/oauth2/authorize?client_id=418944099127918602&redirect_uri=http%3A%2F%2Flocalhost%2Fauth%2Freturn&response_type=code&scope=identify%20guilds"
       // prod:
-      // href="https://discordapp.com/api/oauth2/authorize?client_id=418944099127918602&redirect_uri=http%3A%2F%2Fpbot.jmcountryman.com%2Fauth%2Freturn&response_type=code&scope=identify%20guilds"
+      href="https://discordapp.com/api/oauth2/authorize?client_id=418944099127918602&redirect_uri=http%3A%2F%2Fpbot.jmcountryman.com%2Fauth%2Freturn&response_type=code&scope=identify%20guilds"
     >
       Sign in with
       <img alt="Discord" src={DiscordLogo} />
